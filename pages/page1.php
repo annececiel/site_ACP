5e3f5cb5d02f6c40f11f99a701545ed4e1a9926b
annececiel <61051151+annececiel@users.noreply.github.com>
2020-02-14 13:24:56 +0100

    Create page1.php

---
+++ pages/page1.php
@@ -0,0 +1,13 @@
+<div id="corps" class="container-fluid">
+    <div class="page-header1">
+
+        <div class="row Kslider">
+        <?php include("/includes/karousel.php"); ?>
+        </div>
+
+    </div>
+
+    <h1>Welcome to Kase One!</h1>
+    <?php include("/includes/kontent1.php"); ?>
+    <!--<p class="text-center">Page 1</p>-->
+</div>
