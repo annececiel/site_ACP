d91c73357740c592110c190195dfd4c75a6fe312
annececiel <61051151+annececiel@users.noreply.github.com>
2020-02-15 15:50:21 +0100

    Add files via upload

--- pages/page2.php
+++ pages/page2.php
@@ -1,7 +1,7 @@
 <div id="corps" class="container-fluid">
     <div class="row col-md-12">
         <h1>Contactez-nous</h1>
-        <?php include("/includes/kontact.php"); ?>
+        <?php include(__DIR__."/../includes/kontact.php"); ?>
         
         <!--<p class="text-center">Page 2</p>-->
     </div>
